45c268d15bfced74671e05ee83442d392a5a3634
Will Payne <67875237+blaze005@users.noreply.github.com>
2021-02-20 15:01:36 -0500

    Update setup.py

--- PyPAD-Docs-Python-Edition/setup.py
+++ PyPAD-Docs-Python-Edition/setup.py
@@ -1,7 +1,9 @@
 import os
 
 
-os.system("pip install pyinstaller")
+os.system("pip3 install -r requirements.txt")
+os.system("pip install -r requirements.txt")
+
 print('Setting Up EXE')
 os.system('pyinstaller --onefile --windowed --icon=text.ico PyPAD_Docs.py')
 print('Complete')
