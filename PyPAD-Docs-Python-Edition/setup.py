4cfdb32a4f6b713d20791f32525255048bd18302
Will Payne <67875237+blaze005@users.noreply.github.com>
2021-02-21 21:16:40 -0500

    Delete setup.py

--- PyPAD-Docs-Python-Edition/setup.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-os.system("pip3 install -r requirements.txt")
-os.system("pip install -r requirements.txt")
-
-print('Setting Up EXE')
-os.system('pyinstaller --onefile --windowed --icon=text.ico PyPAD_Docs.py')
-print('Complete')
