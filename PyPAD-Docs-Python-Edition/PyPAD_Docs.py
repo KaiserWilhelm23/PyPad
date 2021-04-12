b6d89ed145bcd582ef958c563f30599d726a2efe
Will Payne <67875237+blaze005@users.noreply.github.com>
2021-04-12 19:13:17 -0400

    Add files via upload

--- PyPAD-Docs-Python-Edition/PyPAD_Docs.py
+++ PyPAD-Docs-Python-Edition/PyPAD_Docs.py
@@ -9,341 +9,6 @@ from tkinter import ttk
 from tkinter.ttk import *
 
 
-def PyPADlite():
-    
-    root = Tk()
-    root.configure(background="green")
-    root.iconbitmap(r'text.ico')
-    root.title("PyPAD Lite")
-    root.geometry('600x600')
-
-
-    global open_status_name
-    open_status_name = False
-
-    global selected
-    selected = False 
-
-    # Creat new file
-    def new_file():
-        my_text.delete('1.0',END)
-        root.title('New Pad - PyPAD')
-
-    
-
-    #open file
-    def open_file():
-        #delete previouse text
-        my_text.delete('1.0',END)
-
-        text_file = filedialog.askopenfilename(title='Open File')
-
-        if text_file:
-            #check if file name
-            global open_status_name
-            open_status_name =  text_file
-        
-    
-        root.title(f'{text_file} - PyPAD')
-        text_file = open(text_file, 'r')
-        stuff = text_file.read()
-        my_text.insert(END, stuff)
-        text_file.close()
-
-    #save file as
-    def save_as_file():
-        text_file = filedialog.asksaveasfilename(defaultextension='.*',title='Save As')
-
-        text_file = open(text_file, 'w')
-        text_file.write(my_text.get(1.0, END))
-        text_file.close()
-
-    #save file
-    def save_file():
-        global open_status_name
-        if open_status_name:
-            text_file = open(open_status_name, 'w')
-            text_file.write(my_text.get(1.0, END))
-            text_file.close()
-            #pop up box
-            messagebox.showinfo('Saved', 'Your File Has Saved')
-
-        else:
-            save_as_file()
-            messagebox.showinfo('Saved', 'Your File Has Saved')
-        
-        
-    #cut text
-    def cut_text(e):
-        global selected
-
-        if my_text.selection_get():
-            # Grab selected text from text box
-            selected = my_text.selection_get()
-            #delete selected text from text box
-            my_text.delete('sel.first','sel.last')
-            
-        
-
-    #copy text
-    def copy_text(e):
-        global selected 
-        if my_text.selection_get():
-            # Grab selected text from text box
-            selected = my_text.selection_get()
-
-    #paste text
-    def paste_text(e):
-   
-       if selected:
-            position = my_text.index(INSERT)
-            my_text.insert(position, selected)
-
-    #bold
-    def bold_it():
-        bold_font = font.Font(my_text, my_text.cget('font'))
-        bold_font.configure(weight='bold')
-
-        current_tags= my_text.tag_names('sel.first')
-
-
-        #config tag
-        my_text.tag_configure('bold',font=bold_font)
-
-        if 'bold' in current_tags:
-            my_text.tag_remove('bold', 'sel.first','sel.last')
-        else:
-            my_text.tag_add('bold','sel.first','sel.last' )
-
-
-    #italics
-    def italics_it():
-        italic_font = font.Font(my_text, my_text.cget('font'))
-        italic_font.configure(slant='italic')
-
-        current_tags= my_text.tag_names('sel.first')
-
-
-        #config tag
-        my_text.tag_configure('italic',font=italic_font)
-
-        if 'italic' in current_tags:
-            my_text.tag_remove('italic', 'sel.first','sel.last')
-        else:
-            my_text.tag_add('italic','sel.first','sel.last' )
-
-
-    def text_color():
-        #colors
-        my_color = colorchooser.askcolor()[1]
-        if my_color:
-            color_font = font.Font(my_text, my_text.cget('font'))
-            current_tags= my_text.tag_names('sel.first')
-
-
-            #config tag
-            my_text.tag_configure('colored',font=color_font, foreground=my_color)
-
-            if 'colored' in current_tags:
-                my_text.tag_remove('colored', 'sel.first','sel.last')
-            else:
-                my_text.tag_add('colored','sel.first','sel.last' )
-    
-    def bg_color():
-         my_color = colorchooser.askcolor()[1]
-         if my_color:
-             my_text.config(bg=my_color)
-
-    def all_text_color():
-        my_color = colorchooser.askcolor()[1]
-        if my_color:
-             my_text.config(fg=my_color)
-
-    def help():
-        pop = Toplevel(root)
-        pop.title('INFO')
-        pop.iconbitmap(r'text.ico')
-        pop.geometry('350x200')
-        pop_label = Label(pop, text='''
-PyPAD Version 5.5 Full Realse:
-PyPAD Name is Copyright PyPAD is OpenSource
--------------------------------------------
-PyPAD Creator: Will Payne AKA NEO
--------------------------------------------
-System Requirements:
-200 MB of storage
-40 MB RAM''').pack()
-        ok = Button(pop, text='Thanks',command=pop.destroy).pack()
-
-
-    def license():
-        pop = Toplevel(root)
-        pop.title("MIT LICENSE")
-        pop.iconbitmap(r'text.ico')
-        pop.geometry('500x500')
-        pop_label = Label(pop,text= '''
-
-MIT License
-
-Will Payne
-
-2021
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-
-
-
-''').pack()
-
-        ok = Button(pop, text='Okay',command=pop.destroy).pack()
-
-
-
-    # create a tool bar frame
-    toolbar_fram = Frame(root)
-    toolbar_fram.pack(fill=X)
-
-
-# create main fram
-    my_frame = Frame(root)
-    my_frame.pack(pady=2)
-
-# scroll bar
-    text_scroll = Scrollbar(my_frame)
-    text_scroll.pack(side=RIGHT, fill=Y)
-
-
-# Text box & font selection
-    global tkvar
-    global fsize
-    tkvar = StringVar(root)
-    choices = { 'Helvetica','Times','MS Sans Serif', 'Symbol', 'Courier'}
-    tkvar.set('MS Sans Serif')
-
-# Font size selector
-    fsize = StringVar(root)
-    sizes = {8,9,10,11,12,14,18,24,30}
-    fsize.set(11)
-
-
-    popupMenu = OptionMenu(toolbar_fram, tkvar, *choices)
-    popupMenu.grid(row = 0, column =3)
-
-    popupMenu2 = OptionMenu(toolbar_fram, fsize, *sizes)
-    popupMenu2.grid(row = 0, column=4)
-
-
-    def change_dropdown(*args): 
-        textFace()
-
-    def size_dropdown(*args):
-        textFace()
-    
-    my_text = Text(my_frame,width=200, height=50, font=("Helvetica",11),selectbackground="light blue", selectforeground="black", undo=True, yscrollcommand=text_scroll.set)
-    my_text.pack()
-    tkvar.trace('w', change_dropdown)
-    fsize.trace('w', size_dropdown)
-
-    def textFace():
-        my_text.configure(font=(tkvar.get(),fsize.get()))
-
-# config scrollbar
-    text_scroll.config(command=my_text.yview)
-
-#Menu
-    my_menu = Menu(root)
-    root.config(menu=my_menu)
-
-# add file menu
-    file_menu = Menu(my_menu, tearoff=False)
-    my_menu.add_cascade(label='File',menu=file_menu)
-    file_menu.add_command(label="New",command=new_file)
-    file_menu.add_command(label="Open",command=open_file)
-    file_menu.add_command(label="Save", command=save_file)
-    file_menu.add_command(label="Save As",command=save_as_file)
-    file_menu.add_separator()
-
-    file_menu.add_separator()
-    file_menu.add_command(label="Exit",command=root.destroy, accelerator="Ctrl+Q")
-
-
-#add edit menu
-    edit_menu = Menu(my_menu,tearoff=False)
-    my_menu.add_cascade(label='Edit',menu=edit_menu )
-    edit_menu.add_command(label="Cut",command=lambda: cut_text(False))
-    edit_menu.add_command(label="Copy",command=lambda: copy_text(False))
-    edit_menu.add_command(label="Paste",command=lambda: paste_text(False))
-    edit_menu.add_separator()
-    edit_menu.add_command(label="Undo", command=my_text.edit_undo,accelerator='(Ctrl+z)')
-    edit_menu.add_command(label="Redo",command=my_text.edit_redo, accelerator='(Ctrl+y)')
-    edit_menu.add_separator()
-
-
-
-#add color menu
-    colors_menu = Menu(my_menu,tearoff=False)
-    my_menu.add_cascade(label='Visuals',menu=colors_menu )
-    colors_menu.add_command(label="Change Selected Text Color",command=text_color )
-    colors_menu.add_command(label="Change All Text Color",command=all_text_color)
-    colors_menu.add_command(label="Background Color",command=bg_color)
-    colors_menu.add_separator()
-
-    colors_menu.add_command(label="BOLD",command=bold_it, font="Bold 10")
-    colors_menu.add_command(label="Italics",command=italics_it)
- #add help menu
-    help_menu = Menu(my_menu,tearoff=False)
-    my_menu.add_cascade(label='Help',menu=help_menu)
-    help_menu.add_command(label="Help",command=help )
-    help_menu.add_command(label="License",command=license )
-
-
-
-# status Bar to Bottom Of app
-#status_bar = Label(root, text='Ready  ',my_menu,anchor=E,)
-#status_bar.pack(ipady=10)
-
-#edit bindings
-    root.bind('<Control-Key-x>',cut_text)
-    root.bind('<Control-Key-c>',copy_text)
-    root.bind('<Control-Key-v>',paste_text)
-    root.bind('<Control-Key-Q>',exit)
-
-
-#buttons
-    undo_button = Button(toolbar_fram, text='<--',command=my_text.edit_undo)
-    undo_button.grid(row=0, column=0,)
-
-    redo_button = Button(toolbar_fram, text='-->',command=my_text.edit_redo)
-    redo_button.grid(row=0, column=1,)
-
-
-
-
-
-
-
-
-
-
-
-    root.mainloop()
 
 
 
@@ -361,7 +26,6 @@ try:
     import time
     import os
     from tkinter.ttk import *
-    from ttkthemes import ThemedStyle
 
 except Exception as e:
     import os
@@ -445,7 +109,7 @@ import pyautogui
 import time
 import os
 from tkinter.ttk import *
-from ttkthemes import ThemedStyle
+
 
 # Starting screen
 def starting_screen():
@@ -461,6 +125,9 @@ def starting_screen():
     
     file = PhotoImage(file =r"text.png")
 
+    def error():
+        messagebox.showwarning(title="Unsupported",message="PyPAD Lite is now unsupported")
+
 
     photoimage1 = Label(image=file)
     photoimage1.pack()
@@ -472,7 +139,7 @@ def starting_screen():
 
     continue1 = Button(window, text="PyPAD Full Version!!",command=kill)
     continue1.pack()
-    button3 = Button(window, text="PyPAd Lite", command=PyPADlite).pack()
+    button3 = Button(window, text="PyPAd Lite", command=error).pack()
 
 
     info = Label(window, text="MIT LICENSE (Open Source) 2021", )
@@ -821,38 +488,7 @@ def search():
 
     root1.mainloop()
 
-def norm():
-    root.mainloop()
-    
-def winxp():
-    style = ThemedStyle(root)
-    style.set_theme("winxpblue")
-    
-def apple():
-    style = ThemedStyle(root)
-    style.set_theme("aquativo")
 
-def girl():
-    style = ThemedStyle(root)
-    style.set_theme("clearlooks")
-
-def wood():
-    style = ThemedStyle(root)
-    style.set_theme("kroc")
-
-def un():
-    style = ThemedStyle(root)
-    style.set_theme("radiance")
-
-    
-def settings():
-    pop = Tk()
-    pop.title("PyPAD Settings")
-    pop.iconbitmap(r'text.ico')
-    pop.geometry('500x500')
-
-    pop.mainloop()
-    
 
 
 # create a tool bar frame
@@ -962,14 +598,7 @@ resources_menu.add_command(label="Python",command=open_python)
 #resources_menu.add_command(label="Settings",command=settings)
 
 
-#add theme menue
-theme_menu = Menu(my_menu,tearoff=False)
-my_menu.add_cascade(label='Themes',menu=theme_menu )
-theme_menu.add_command(label="Windows XP Theme",command=winxp)
-theme_menu.add_command(label="Apple OS Like Theme",command=apple)
-theme_menu.add_command(label="Feminine Theme",command=girl)
-theme_menu.add_command(label="Wood Theme",command=wood)
-theme_menu.add_command(label="Ubuntu Theme",command=un)
+
 
 
 
